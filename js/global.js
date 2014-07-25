3056aaca0f0b69b2e91c4880fa85f17aabc78721
LennorLv <lvjianhua75@126.com>
2014-07-25 23:10:23 +0800

    0726

--- js/global.js
+++ js/global.js
@@ -1,23 +1,28 @@
 // JavaScript Document
 
-
-
-/*window.onload = function() {
+function headerRes() {
+	var bw = document.body.clientWidth;
 	var hh = document.getElementById("pic").offsetHeight;
-	document.getElementById("banner").style.height = hh+"px";
-	mm = hh-105
-	document.getElementById("banner-text").style.paddingTop = mm/2-100+"px";
+	if (bw >= 768){			
+		document.getElementById("banner").style.height = hh+"px";
+		mm = hh-105
+		document.getElementById("banner-text").style.paddingTop = mm/2-100+"px";
+			}
+			//页面宽度大于768时banner的布局
+	else {
+		document.getElementById("banner").style.height = hh+"px";
+		mm = hh-50
+		document.getElementById("banner-text").style.paddingTop = mm/2+"px";
+		}
+		//页面宽度小于768时banner的布局
 	}
-window.onresize = function() {
-	var hh = document.getElementById("pic").offsetHeight;
-	document.getElementById("banner").style.height = hh+"px";
-	mm = hh-105
-	document.getElementById("banner-text").style.paddingTop = mm/2-100+"px";
-	}
-*/
 
 
-//页面载入时计算页面布局
+window.onload = headerRes;
+window.onresize = headerRes;
+
+
+/*//页面载入时计算页面布局
 window.onload = function() {
 	var bw = document.body.clientWidth;
 	var hh = document.getElementById("pic").offsetHeight;
@@ -48,9 +53,9 @@ window.onresize = function() {
 			}
 			//页面宽度大于768时banner的布局
 	else {
-		document.getElementById("banner").style.height = hh-50+"px";
+		document.getElementById("banner").style.height = hh+"px";
 		mm = hh-50
 		document.getElementById("banner-text").style.paddingTop = mm/2+"px";
 		}
 		//页面宽度大于768时banner的布局
-	}
+	}*/
